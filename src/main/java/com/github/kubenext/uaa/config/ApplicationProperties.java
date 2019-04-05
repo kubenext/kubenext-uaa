31b75bd6d916de07238dcda44dc2b8abd1833084
lishangjin <li.shangjin@icloud.com>
2019-04-06 01:25:29 +0800

    :construction: 正在进行中

---
+++ src/main/java/com/github/kubenext/uaa/config/ApplicationProperties.java
@@ -0,0 +1,10 @@
+package com.github.kubenext.uaa.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author lishangjin
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+}
