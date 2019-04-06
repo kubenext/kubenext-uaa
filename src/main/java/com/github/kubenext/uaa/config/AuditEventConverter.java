7175a807c4c338575867bdf78f05a95e70217b9b
lishangjin <li.shangjin@icloud.com>
2019-04-06 08:21:34 +0800

    :arrow_down: 降级为1.8，11在监听jmx时有NPE

---
+++ src/main/java/com/github/kubenext/uaa/config/AuditEventConverter.java
@@ -0,0 +1,65 @@
+package com.github.kubenext.uaa.config;
+
+import com.github.kubenext.uaa.domain.PersistentAuditEvent;
+import org.springframework.boot.actuate.audit.AuditEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+/**
+ * @author shangjin.li
+ * @date Created in 01:40 2019-04-06
+ * @description AuditEventConverter
+ */
+@Component
+public class AuditEventConverter {
+
+    public List<AuditEvent> convertToAuditEvent(Iterable<PersistentAuditEvent> persistentAuditEvents) {
+        if (persistentAuditEvents == null) {
+            return Collections.emptyList();
+        }
+        List<AuditEvent> auditEvents = new ArrayList<>();
+        persistentAuditEvents.forEach(persistentAuditEvent -> auditEvents.add(convertToAuditEvent(persistentAuditEvent)));
+        return auditEvents;
+    }
+
+    public AuditEvent convertToAuditEvent(PersistentAuditEvent persistentAuditEvent) {
+        if (persistentAuditEvent == null) {
+            return null;
+        }
+        return new AuditEvent(
+            persistentAuditEvent.getEventDate(),
+            persistentAuditEvent.getPrincipal(),
+            persistentAuditEvent.getEventType(),
+            convertDataToObjects(persistentAuditEvent.getData())
+        );
+    }
+
+    public Map<String, Object> convertDataToObjects(Map<String, String> data) {
+        Map<String, Object> results = new HashMap<>(data.size());
+        if (data != null) {
+            data.forEach((k,v) -> {
+                results.put(k, v);
+            });
+        }
+        return results;
+    }
+
+    public Map<String, String> convertDataToStrings(Map<String, Object> data) {
+        Map<String, String> results = new HashMap<>(data.size());
+        if (data != null) {
+            data.forEach((k,v) -> {
+                if (v instanceof WebAuthenticationDetails) {
+                    WebAuthenticationDetails authenticationDetails = (WebAuthenticationDetails) v;
+                    results.put("remoteAddress", authenticationDetails.getRemoteAddress());
+                    results.put("sessionId", authenticationDetails.getSessionId());
+                } else {
+                    results.put(k, Objects.toString(v));
+                }
+            });
+        }
+        return results;
+    }
+
+}
---
+++ src/main/java/com/github/kubenext/uaa/domain/PersistentAuditEvent.java
@@ -0,0 +1,106 @@
+package com.github.kubenext.uaa.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Persist AuditEvent managed by the Spring Boot actuator.
+ *
+ * @author shangjin.li
+ * @date Created in 01:44 2019-04-06
+ */
+@Entity
+public class PersistentAuditEvent implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(length = 50, nullable = false)
+    private String principal;
+
+    @NotNull
+    @Column(nullable = false)
+    private Instant eventDate;
+
+    private String eventType;
+
+    @ElementCollection
+    @MapKeyColumn(name = "name")
+    @Column(name = "value")
+    @CollectionTable(name = "persistent_audit_evt_data", joinColumns = @JoinColumn(name = "id"))
+    private Map<String, String> data = new HashMap<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(String principal) {
+        this.principal = principal;
+    }
+
+    public Instant getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Instant eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public Map<String, String> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, String> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PersistentAuditEvent that = (PersistentAuditEvent) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "PersistentAuditEvent{" +
+                "id=" + id +
+                ", principal='" + principal + '\'' +
+                ", eventDate=" + eventDate +
+                ", eventType='" + eventType + '\'' +
+                '}';
+    }
+}
